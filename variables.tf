1d9ac25d13baec3672f66b7b39c2285e346dedd6
Vedant Pareek <dunefro@gmail.com>
2023-08-11 15:05:47 +0530

    Added support for shim in resource group

--- variables.tf
+++ variables.tf
@@ -1,9 +1,23 @@
+##################################################################################
+## Variables
+##################################################################################
+
+### Shim
+variable "shim" {
+  description = "If true will not create the resource group"
+  type        = bool
+  default     = false
+}
+
+## Non Shim
 variable "name" {
   description = "Resource Group Name"
+  type        = string
 }
 
 variable "location" {
   description = "Location"
+  type        = string
 }
 
 variable "tags" {
